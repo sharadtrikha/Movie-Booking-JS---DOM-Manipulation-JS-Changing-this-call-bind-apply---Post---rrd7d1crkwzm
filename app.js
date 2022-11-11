913dd49f4426e3439efd392a9b0a506d8ee8ae19
Sharad Trikha <sharadtrikha@gmail.com>
2022-11-11 22:46:16 +0530

    Code complete 2nd day - movie booking app

--- app.js
+++ app.js
@@ -2,6 +2,14 @@ import {fetchMovieAvailability,fetchMovieList} from "./api.js";
 
 // Selectors
 const mainElement = document.querySelector("main");
+const bookerElement = document.querySelector("#booker");
+const bookerGridElement = document.querySelector("#booker-grid-holder");
+const bookTicketBtn = document.querySelector("#book-ticket-btn");
+
+bookTicketBtn.addEventListener("click", onBookTicketClickHandler);
+
+// Store Selected Seats
+let selectedSeats = [];
 
 // Task : Convert HTML String to HTML DOM ELEMENT
 const convertToHtmlDom = (htmlInStringFormat) => {
@@ -14,18 +22,119 @@ const convertToHtmlDom = (htmlInStringFormat) => {
 // Create Loader
 const loader = convertToHtmlDom(`<div class="loader">Loading .........</div>`);
 
+const onSeatClick = (event) => {
+    event.target.classList.toggle("selected-seat");
+
+    // logic : if element is having class selected-seat, then, it needs to be push
+    // onto selectedSeats Array and if it is not having, then, remove that seat from 
+    // selectedSeats array
+
+    if(event.target.classList.contains("selected-seat")) {
+        selectedSeats.push(event.target.innerText);
+    } else {
+        selectedSeats = selectedSeats.filter(seat => seat !== event.target.innerText);
+    }
+
+    // Add or Remove Book Ticket button from UI
+    if(selectedSeats.length > 0) {
+        bookTicketBtn.classList.remove("v-none");
+    } else {
+        bookTicketBtn.classList.add("v-none");
+    }
+
+}
+
+
+const renderTheatreLayout = (listOfUnavailableSeats = [],seatNoOffset=1) => {
+
+    // make a grid of 4*3
+    const grid = convertToHtmlDom(`<div class="booking-grid"></div>`);
+
+    // insert Grid elements basically theatre seats
+    let theatreSeats = "";
+
+    for(let i = 0; i < 12; i++) {
+        theatreSeats = theatreSeats + 
+        `<div id="booking-grid-${i+seatNoOffset}" class="grid-cell ${listOfUnavailableSeats.includes(i+seatNoOffset) ? "unavailable-seat": "available-seat"}">${i+seatNoOffset}</div>`
+    }
+
+    grid.innerHTML = theatreSeats;
+    bookerGridElement.appendChild(grid);
+
+    document.querySelectorAll(".grid-cell").forEach(cell => cell.addEventListener("click", onSeatClick));
+
+}
+
+const renderSuccessMessage = (mobileNumber, email) => {
+    const successMsg = convertToHtmlDom(`<div id="Success">
+        <h4>Booking Details</h4>
+        <div>Seats: ${selectedSeats.join(",")}</div>
+        <div>Phone number: ${mobileNumber}</div>
+        <div>Email: ${email}</div>
+    </div>`);
+
+    bookerElement.appendChild(successMsg);
+
+}
+
+const onPurchaseBtnClickHandler = (event) => {
+    event.preventDefault();
+    const mobileNumber = document.querySelector("#mobile").value;
+    const email = document.querySelector("#email_movie").value;
+    bookerElement.innerHTML = "";
+    renderSuccessMessage(mobileNumber,email);
+}
+
+function renderConfirmPurchaseForm() {
+    const form = convertToHtmlDom(`
+    <div id="confirm-purchase">
+        <h3>Confirm your booking for seat numbers:${selectedSeats.join(",")}</h3>
+        <form id="customer-detail-form">
+        <div>
+            <label for="email_movie">Email: </label>
+            <input type="email" id="email_movie" required />
+            </div>
+            <div>
+            <label for="mobile">Phone Number : </label>
+            <input type="tel" id="mobile" required />
+            </div>
+            <button id="movie_submit_btn" type="submit">Purchase</button>
+        </form>
+    </div>
+    `);
+
+    bookerElement.appendChild(form);
+
+    document.querySelector("form").addEventListener("submit", onPurchaseBtnClickHandler);
+}
+
+function onBookTicketClickHandler () {
+    bookerElement.innerHTML = "";
+    renderConfirmPurchaseForm();
+}
+
 const renderMovieTheatre = (event) => {
     event.preventDefault();
     console.log(event.target.innerText);
 
     const movieName = event.target.innerText ? event.target.innerText : event.target.parentElement.innerText;
 
-    fetchMovieAvailability(movieName).then((result) => {
-        console.log(result);
+    bookerElement.appendChild(loader);
+    fetchMovieAvailability(movieName).then((listOfUnavailableSeats) => {
+        console.log(listOfUnavailableSeats);
+
+        loader.remove();
 
         // make h3 element of booker div visible
         const bookerElementHeader = document.querySelector("#booker h3");
         bookerElementHeader.classList.toggle("v-none");
+        
+        // render Theatre layout view
+        renderTheatreLayout(listOfUnavailableSeats);
+        renderTheatreLayout(listOfUnavailableSeats, 13);
+
+
+
 
     })
 }
